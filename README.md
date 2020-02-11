3a20c6a6b33ecd8828b563e0d81fb2c31f13e8fc
Sevha Amos Vukeya <vukeyasa@explore-datascience.net>
2020-02-11 13:52:55 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#mypackage
+this library is created as package for the predict work
+
+## building this package locally
+'python setup.py sdist'
+
+## updating this package from GitHub
+'pip install git+https://github.com/James-leslie/example-python-package.git'
+
+## updating this package from GitHub
+'pip install --upgrade git+https://github.com/James-Leslia/example-python-package.git'
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/vuks01/mypackage',
+    author='vuks01',
+    author_email='vukeyasa@gmail.com'
+)
